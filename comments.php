e51af81ff1036f70385849ae2bca4969acd81814
therakib7 <therakib7@gmail.com>
2020-09-22 00:25:02 +0600

    comment form and comments full match with html :)

--- comments.php
+++ comments.php
@@ -17,57 +17,44 @@ if ( post_password_required() ) {
 	return;
 } ?>
 <?php if ( have_comments() ) : ?>
-<div id="comments" class="comments-area m-40px-t m-50px-b comments">
-	<div class="comments-main-content">
-		<div class="row">
-		    <div class="col-md-12">
-		        <h3 class="comments-title">
-		        	<?php /* translators: %s: search term */ ?>
-		        	<?php echo esc_html( sprintf( _nx( '%s Comment', '%s Comments', get_comments_number(), 'comments title', 'mombo' ), number_format_i18n( get_comments_number() ) ) ); ?>
-		        
-		        </h3><!--/.comments-title-->
-		    </div><!--/.col-md-12-->
-		</div><!--/.row-->
- 
-		<div class="row">
-		    <div class="col-md-12">
-		         <ol class="comment-list">
-		         	<?php
-		         		wp_list_comments( array(
-		         			'style'      => 'ol',
-		         			'short_ping' => true,
-		         			'callback' => 'mombo_comment_list',
-		         		) );
-		         	?>
-		         </ol><!-- .comment-list -->
-		    </div><!--/.col-md-12-->
-		</div><!--/.row-->
-
-		<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
-			<nav id="comment-nav-below" class="navigation comment-navigation">
-				<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'mombo' ); ?></h2>
-				<div class="nav-links">
-					<?php
-						$allowed_html_array = array(
-					        'span' => array()
-					    );
-					?>
-					<div class="nav-previous"><?php previous_comments_link( wp_kses( __( '<span>&laquo;</span> Older Comments', 'mombo' ), $allowed_html_array ) ); ?></div>
-					<div class="nav-next"><?php next_comments_link( wp_kses( __( 'Newer Comments <span>&raquo;</span>', 'mombo' ), $allowed_html_array ) ); ?></div>
-				</div><!-- .nav-links -->
-			</nav><!-- #comment-nav-below -->
-		<?php endif; // check for comment navigation ?>
-
-		<?php
-			// If comments are closed and there are comments, let's leave a little note, shall we?
-			if ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
-			<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'mombo' ); ?></p>
-		<?php endif; // have_comments() ?>
-	</div><!-- /.comments-main-content -->
-</div><!-- #comments -->
-<?php endif; ?>
-<div class="card gray-bg">
-	<div class="card-body">
-		<?php comment_form(); ?>
+<div class="comments-area m-40px-t m-50px-b">
+	<div class="border-bottom-1 border-color-gray p-10px-b m-25px-b">
+		<h4 class="m-0px">
+			<?php /* translators: %s: search term */ ?>
+					<?php echo esc_html( sprintf( _nx( '%s Comment', '%s Comments', get_comments_number(), 'comments title', 'mombo' ), number_format_i18n( get_comments_number() ) ) ); ?>
+		</h4>
 	</div>
+	<ul class="comment-list">
+		<?php
+			wp_list_comments( array(
+				'style'      => 'li',
+				'short_ping' => true,
+				'callback' => 'mombo_comment_list',
+			) );
+		?>
+	</ul>
+	<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
+		<nav id="comment-nav-below" class="navigation comment-navigation">
+			<h2 class="screen-reader-text"><?php esc_html_e( 'Comment navigation', 'mombo' ); ?></h2>
+			<div class="nav-links">
+				<?php
+					$allowed_html_array = array(
+						'span' => array()
+					);
+				?>
+				<div class="nav-previous"><?php previous_comments_link( wp_kses( __( '<span>&laquo;</span> Older Comments', 'mombo' ), $allowed_html_array ) ); ?></div>
+				<div class="nav-next"><?php next_comments_link( wp_kses( __( 'Newer Comments <span>&raquo;</span>', 'mombo' ), $allowed_html_array ) ); ?></div>
+			</div><!-- .nav-links -->
+		</nav><!-- #comment-nav-below -->
+	<?php endif; // check for comment navigation ?>
+
+	<?php
+	// If comments are closed and there are comments, let's leave a little note, shall we?
+	if ( ! comments_open() && '0' != get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) : ?>
+		<p class="no-comments"><?php esc_html_e( 'Comments are closed.', 'mombo' ); ?></p>
+	<?php endif; // have_comments() ?>
 </div>
+						
+<?php endif; ?>
+
+<?php comment_form(); ?>
--- inc/functions/wp-comment-section-modify.php
+++ inc/functions/wp-comment-section-modify.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Modify comment template
+ *
+ * @package Mombo
+ * @since 1.0
+ */
+
 function mombo_move_comment_field_to_bottom( $fields ) {
 	$comment_field = $fields['comment'];
 	unset( $fields['comment'] );
@@ -28,6 +35,7 @@ function mombo_comment_form($args) {
 	$args['class_form'] = "row";
 	$args['id_submit'] = "submit";
 	$args['class_submit'] = "m-btn m-btn-radius m-btn-theme";
+	$args['class_container'] = "comment-respond card gray-bg card-body";
 	$args['submit_field'] = '<div class="col-md-12"><p class="form-submit">%1$s %2$s</p></div>';
 	$args['logged_in_as'] = sprintf(
 		'<div class="col-md-12"><p class="logged-in-as">%s</p></div>',
@@ -43,7 +51,7 @@ function mombo_comment_form($args) {
 		)
 	);
 	$args['name_submit'] = "submit";
-	$args['title_reply'] = wp_kses( __( '<h4 class="m-30px-b">Leave a Reply</h4>', 'mombo' ), Mombo_Static::html_allow() );
+	$args['title_reply'] = wp_kses( __( '<span>Leave a Reply</span>', 'mombo' ), Mombo_Static::html_allow() );
 
 	/* translators: %s: Extra words for comment title */
 	$args['title_reply_to'] = wp_kses( __( 'Leave a Reply to %s', 'mombo' ), Mombo_Static::html_allow() );
@@ -71,37 +79,41 @@ function mombo_comment_list($comment, $args, $depth) {
 <<?php echo wp_kses_post( $tag ); ?> <?php comment_class( empty( $args['has_children'] ) ? '' : 'parent' ) ?> id="comment-<?php comment_ID() ?>">
 <?php if ( 'div' != $args['style'] ) : ?>
 <div id="div-comment-<?php comment_ID() ?>" class="comment-body">
-<?php endif; ?>
-    <div class="comment-meta">
-        <div class="comment-author vcard">
-        	<?php if(get_avatar($comment, $size='60')) { ?>
-            <div class="author-img">
-                <?php echo get_avatar($comment, $size='60'); ?>	
-            </div>
-            <?php } ?>
-        </div><!--/.comment-author-->
-        <div class="comment-metadata">
-        	<b class="author">
-                <?php /* translators: %1$s: Comments Authors */ ?>
-                <?php printf( esc_html__( ' %1$s ', 'mombo' ), get_comment_author_link() ); ?></b>
-            <span class="date">
-            	<?php
-            	    /* translators: Comments date, edit link. */
-            		printf( esc_html__('%1$s at %2$s','mombo'), get_comment_date(),  get_comment_time() ); ?><?php edit_comment_link( esc_html__( '(Edit)','mombo' ), '  ', '' );
-            	?>
-            </span>
-        </div><!--/.comment-metadata-->
-    </div><!--/.comment-meta-->
-    <div class="comment-details">
-        <div class="comment-content">
-            <?php comment_text(); ?>
-            <?php if ( $comment->comment_approved == '0' ) : ?>
-            	<p><em class="comment-awaiting-moderation"><?php esc_html_e( 'Your comment is awaiting moderation.','mombo' ); ?></em></p>
-            <?php endif; ?>
-        </div><!--/.comment-content-->
-        <?php comment_reply_link( array_merge( $args, array( 'add_below' => $add_below, 'depth' => $depth, 'max_depth' => $args['max_depth'] ) ) ); ?>
-    </div><!-- /.comment-details-->
+<?php endif; ?> 
+	
+	<div class="comment-meta d-flex align-items-center">
+		<div class="comment-author">
+			<?php echo get_avatar($comment, $size='50'); ?>	
+		</div>
+		<div class="comment-metadata">
+			<div class="c-name"><?php printf( esc_html__( ' %1$s ', 'mombo' ), get_comment_author_link() ); ?></div>
+			<span class="c-date">
+				<?php
+					/* translators: Comments date, edit link. */
+					printf( esc_html__('%1$s at %2$s','mombo'), get_comment_date(),  get_comment_time() ); ?><?php edit_comment_link( esc_html__( '(Edit)','mombo' ), '  ', '' );
+				?> 
+			</span>
+		</div>
+	</div>
+	<div class="comment-content">
+		<?php comment_text(); ?>
+		<?php if ( $comment->comment_approved == '0' ) : ?>
+			<p><em class="comment-awaiting-moderation"><?php esc_html_e( 'Your comment is awaiting moderation.','mombo' ); ?></em></p>
+		<?php endif; ?>
+	</div>
+	
+	<div class="comment-reply"> 
+		<?php 
+			$new_class = 'm-btn m-btn-t-theme m-btn-radius m-btn-sm';
+			echo preg_replace( '/comment-reply-link/', 'comment-reply-link ' . $new_class, 
+				get_comment_reply_link(array_merge( $args, array(
+					'add_below' => $add_below, 
+					'depth' => $depth, 
+					'max_depth' => $args['max_depth']))), 1 ); 
+		?>
+	</div> 
+					
 	<?php if ( 'div' != $args['style'] ) : ?>
 	</div><!-- /.comment-body -->
-	<?php endif; ?>
-<?php }
+	<?php endif; 
+}
