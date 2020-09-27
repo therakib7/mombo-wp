a5546b264098866842d250532d10eb3a5dfb6d67
therakib7 <therakib7@gmail.com>
2020-09-28 00:25:56 +0600

    change single portfoli

--- single-portfolio.php
+++ single-portfolio.php
@@ -1,50 +1,69 @@
 <?php
 /**
- * The template for displaying all portfolio single posts.
- *
+ * The template for single portfolio 
+ * 
  * @package Mombo
  * @since 1.0
  */
-get_header(); ?>
-<!-- Portfolio Single Block
-================================================== -->
-<section class="portfolio-page-block pd-t-220 pd-b-135">
-    <div class="container portfolio-single-page">
-        <div class="row">
-        	<?php while ( have_posts() ) : the_post(); ?>
-            <div class="col-md-12">        
-                <div class="sh-elementor-content clearfix">
-                    <?php the_content(); ?>
-                </div>
-            </div><!-- /col-md-12 -->
-    	    <?php endwhile; ?>
-        </div><!-- /.row -->
-    </div><!-- /.container -->
+get_header();
 
-    <div class="container portfolio-pagination">
-        <div class="row align-items-center">
-        	<?php 
-        	$prev_post = get_previous_post();
-        	if($prev_post) {
-        	   $prev_title = strip_tags(str_replace('"', '', $prev_post->post_title)); ?>
-				<div class="col text-left">
-				    <a href="<?php echo esc_url( get_permalink($prev_post->ID) ); ?>" class="older-posts" title="<?php echo esc_attr( $prev_title ); ?>"><i class="gra-arrow-left"></i><span class="navigation-text"><?php esc_html_e('Older Posts', 'mombo'); ?></span></a>
-				</div>
-        	<?php } ?>
-            
-            <div class="col text-center">
-                <a href="<?php echo esc_url( get_post_type_archive_link( 'portfolio' ) ); ?>" class="portfolio-archive-link"><i class="fa fa-braille"></i></a>
-            </div>
-
-            <?php 
-            $next_post = get_next_post();
-            if($next_post) {
-               $next_title = strip_tags(str_replace('"', '', $next_post->post_title)); ?>
-	            <div class="col text-right">
-	                <a href="<?php echo esc_url( get_permalink($next_post->ID) ); ?>" class="newer-posts" title="<?php echo esc_attr($next_title); ?>"><span class="navigation-text"><?php esc_html_e('Newer Posts', 'mombo'); ?></span> <i class="gra-arrow-right"></i></a>
-	            </div>
-            <?php } ?>
+$elemetor = get_post_meta( get_the_ID(), '_elementor_edit_mode');
+if( $elemetor ) : 
+    if ( have_posts() ) : 
+        while ( have_posts() ) : the_post(); ?> 
+        <div class="mombo-page-builder clearfix">
+            <?php the_content(); ?>
         </div>
-    </div><!-- /.container -->
-</section><!-- /.blog-block -->
+        <?php 
+        endwhile; 
+    endif; 
+else: ?>
+    <!-- Main -->
+    <main>
+        <!-- Page Title -->
+        <section class="section bg-center bg-cover bg-fixed effect-section" style="background-image: url(<?php echo get_template_directory_uri(); ?>/assets/img/banner-bg-5.jpg);">
+            <div class="mask theme-bg opacity-9"></div>
+            <div class="container">
+                <div class="row justify-content-center p-50px-t">
+                    <div class="col-lg-8 text-center"> 
+                        <?php the_title( '<h2 class="white-color h1 m-20px-b">', '</h2>' ); ?> 
+                        <ol class="breadcrumb white justify-content-center">
+                            <li><a href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php esc_html_e('Home', 'mombo'); ?></a></li>
+                            <li class="active"><?php the_title(); ?></li>
+                        </ol>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End Page Title -->
+
+        <!-- Section -->
+        <section class="section">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-9">
+                        <?php while ( have_posts() ) : the_post(); ?>
+                        <div class="card">
+                            <div class="card-body"> 
+                                <?php 
+                                    the_content(); 
+                                    mombo_wp_link_pages(); 
+                                ?>
+                            </div>
+                            <?php 
+                                // If comments are open or we have at least one comment, load up the comment template.
+                                if ( comments_open() || get_comments_number() ) :
+                                    comments_template();
+                                endif;
+                            ?> 
+                        </div>
+                            <?php endwhile; ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End Section -->
+    </main>
+    <!-- End Main -->
+<?php endif; ?>
 <?php get_footer(); ?>
