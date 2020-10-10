07d0797049642cb3c1ee102d761dc6553c26bc52
therakib7 <therakib7@gmail.com>
2020-10-10 21:23:19 +0600

    fix conflit

--- template-parts/header/one.php
+++ template-parts/header/one.php
@@ -44,7 +44,7 @@ if ( get_post_meta(get_the_ID(), 'mombo_mb_header_part', true) == 'hide' ) retur
                         // )); 
                     ?>
                 </div> -->
-<<<<<<< HEAD
+ 
                 <div class="overlay">
                     <div id="main-menu">
                         <?php 
@@ -54,19 +54,7 @@ if ( get_post_meta(get_the_ID(), 'mombo_mb_header_part', true) == 'hide' ) retur
                                 'theme_location' => 'header-menu',  
                             )); 
                         ?> 
-                    </div>
-=======
-                <div id="main-menu">
-                    <?php 
-                        wp_nav_menu ( array(
-                            'container_class' => '',
-                            'container'=> '',
-                            'theme_location' => 'header-menu',  
-                            'walker' => new Mombo_Custom_Walker() ,
-                            'fallback_cb'       => 'Mombo_Custom_Walker::fallback_header_menu', 
-                        )); 
-                    ?>
->>>>>>> 1a38b44eaf6ed546dfb91f030c348834b5fd543b
+                    </div> 
                 </div>
                 <div class="extra-menu d-flex align-items-center">
                     <div class="d-none d-md-block h-btn m-35px-l">
